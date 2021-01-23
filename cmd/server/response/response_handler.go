03d3709bd4beb66c7de8cc43b6fb2213e233e8de
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-01-24 01:28:57 +0900

    If resource is not found, server response 404 status.

--- cmd/server/response/response_handler.go
+++ cmd/server/response/response_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/atsushi-kitazawa/http_server/cmd/server/request"
 	"io/ioutil"
 	"net"
+	"strings"
 )
 
 func Response(conn *net.TCPConn, req request.Request) {
@@ -21,16 +22,25 @@ func Response(conn *net.TCPConn, req request.Request) {
 
 func readResource(req request.Request) string {
 	if req.Resource != "/" {
-		data, err := ioutil.ReadFile("../../" + req.Resource)
+		data, err := ioutil.ReadFile(strings.Trim(req.Resource, "/"))
 		if err != nil {
-			panic(err)
+			return notFoundResponse()
 		}
 		return string(data)
 	} else {
-		data, err := ioutil.ReadFile("../../index.html")
+		data, err := ioutil.ReadFile("index.html")
 		if err != nil {
 			panic(err)
 		}
 		return string(data)
 	}
+}
+
+func notFoundResponse() string {
+	var body bytes.Buffer
+	body.WriteString("HTTP/1.1 404 Not Found")
+	body.WriteString("Content-Type: text/html")
+	body.WriteString("\n")
+	body.WriteString("<html><body>page not found.</body></html>")
+	return body.String()
 }
