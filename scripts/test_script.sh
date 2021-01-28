7fab106f95435463292db156f3a5ce9842ffc471
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-01-29 02:04:30 +0900

    modify test_script

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -11,3 +11,7 @@ echo
 echo "POST [http://localhost:9999/pages/hello.html]"
 curl -X POST -d key1=valu1 -d key2=value2 http://localhost:9999/pages/hello.html
 echo
+
+echo "GET [http://localhost:9999/pages/auth_dir/hello.html]"
+curl -u test:password -i -s http://localhost:9999/pages/hello.html
+echo
