ffef865dee6a7ad14c720726895a43e8addfce6a
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-02-03 01:08:06 +0900

    add header.go header.go is determine content-type

---
+++ cmd/server/header/headers.go
@@ -0,0 +1,24 @@
+package header
+
+import (
+	"strings"
+
+	"github.com/atsushi-kitazawa/http_server/cmd/server/request"
+)
+
+// [key, value] = [extension, content-type]
+var content_type map[string]string = loadContentType()
+
+func loadContentType() map[string]string {
+    m := make(map[string]string)
+    m[".html"] = "text/html"
+    m[".js"] = "text/javascript"
+    m[".css"] = "text/css"
+    return m
+}
+
+func DetermineContentType(req request.Request) string {
+    periodPos := strings.LastIndex(req.Resource, ".")
+    extension := req.Resource[periodPos:]
+    return content_type[extension]
+}
---
+++ cmd/server/header/headers_test.go
@@ -0,0 +1,39 @@
+package header
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/atsushi-kitazawa/http_server/cmd/server/request"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+    fmt.Println("before")
+    loadContentType()
+
+    code := m.Run()
+
+    fmt.Println("after")
+    os.Exit(code)
+}
+
+func TestDetermineContentType(t *testing.T) {
+    fmt.Println("TestDetermineContentType")
+
+    var req request.Request
+    req.Method = "GET"
+    req.Version = "HTTP/1.1"
+    req.Resource = "/pages/hello.html"
+    result := DetermineContentType(req)
+    assert.Equal(t, "text/html", result)
+
+    req.Resource = "/hello.css"
+    result = DetermineContentType(req)
+    assert.Equal(t, "text/css", result)
+
+    req.Resource = "hello.js"
+    result = DetermineContentType(req)
+    assert.Equal(t, "text/javascript", result)
+}
